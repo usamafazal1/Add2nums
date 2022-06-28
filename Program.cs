bb4e730575e928200794fbf0f8457be41a81e53b
Usama Fazal <Usama Fazal@WIN-VOL3D49H6UK>
2022-06-29 00:41:32 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuestionNo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input two numbers, separately 
+            Console.WriteLine("Please enter the first number:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            // create a variable C and add variables A,B
+            int C = A + B;
+
+            // string concatenate to represent the final statement 
+            Console.WriteLine("The sum of the two numbers is: " + C + " !");
+
+        }
+    }
+}
